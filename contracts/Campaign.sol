6bf5458cb624c9ba171fc99796a5d49a23b93afe
liebeskind <daniel.liebeskind@gmail.com>
2018-08-16 13:35:51 -0700

    Campaign creator can now create a request.  Also adds a request struct (and array).

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -9,6 +9,16 @@ contract Campaign {
     uint public totalContributions; // Track total contributions, which will differ from this.balance if funds are distributed.
     uint public approversCount; // How many approvers are there.  Less important than totalContributions, but still interesting.
 
+    struct Request {
+        string description; // Description of the request.  This cannot be changed later, which prevents bait-and-switch.
+        uint value; // Quantity of funds requested for distribution.
+        address recipient; // Address of intended recipient of the funds.
+        bool complete; // Whether this request is complete.
+        string databaseKey; // Location of other request-specific assets like images, videos, etc.
+    }
+    
+    Request[] public requests; // Array of requests.
+
     /**
       * @title Initialize the campaign and set total contributions to 0.
       * @param minimum min amount contributor can contribute to this campaign.
@@ -24,7 +34,7 @@ contract Campaign {
     }
 
     /**
-      * @title contribute wei to the campaign.
+      * @title Contribute wei to the campaign.
       * @param amount of wei contributed to the campaign.
       * @payable this function will receive eth/wei.
       * @public can call this function external to this contract
@@ -38,4 +48,24 @@ contract Campaign {
         approversCount++;
     }
 
+    /**
+      * @title Request for use of funds created by campaign owner.  
+      	@dev The request requires no more than 15% of contributors (by % of total contribution) to vote against it for approval.
+      * @param description of the request.
+      * @param value amount of wei/eth the request is asking to be distributed.
+      * @param recipient address the funds will automatically be sent to if the request is approved.
+      * @param databaseKey for IPFS or other database containing request assets (like images/videos/etc).
+      * @restricted modifier defined above that only allows campaign creator to call this function.
+      * @public can call this function external to this contract.
+    */
+    function createRequest(string description, uint value, address recipient, string databaseKey) public restricted {
+        Request memory newRequest = Request({
+           description: description,
+           value: value,
+           recipient: recipient,
+           complete: false,
+           databaseKey: databaseKey
+        });
+        requests.push(newRequest);
+    }
 }
---
+++ design_pattern_decisions.md
@@ -0,0 +1,7 @@
+#Design Patterns#
+
+-   Voting based on total contribution
+
+-   Storing request description and recipient address on the blockchain and not allowing alterations.
+
+-   Emergency stop
